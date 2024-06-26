1e373b17d5da3fd1fdbde0e6719b7194d4650872
Bakary Sylla <syllabakary2002@gmail.com>
2024-06-25 19:46:49 -0700

    new component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
 import { useState } from "react";
+import TodoList from "./TodoList";
 
 // import "./App.css";
-var todoList = [
-  {
-    id: 1,
-    title: "Assignment not start",
-  },
-  {
-    id: 2,
-    title: "Assignment start",
-  },
-  {
-    id: 3,
-    title: "Assignment completed",
-  },
-];
 
 function App() {
   return (
     <>
       <div>
         <h1>Todo List</h1>
-        <ul>
-          {todoList.map(function (todolists) {
-            return (
-              <li key={todoList.id}>
-                <span>{todolists.id + " "} </span>
-                <span>{todolists.title}</span>
-              </li>
-            );
-          })}
-        </ul>
+        <TodoList />
       </div>
     </>
   );
