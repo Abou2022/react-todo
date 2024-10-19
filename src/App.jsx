4b34f3b7e1ed6463ff8317b83e720196dd3e8e64
Bakary Sylla <syllabakary2002@gmail.com>
2024-10-19 16:50:35 -0500

    sort by time added and sort by title updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,31 @@ import AddTodoForm from "./components/AddTodoForm";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function sortTodoAscending(objectA, objectB) {
-  if (objectA < objectB) {
-    return 1;
-  } else if (objectA > objectB) {
-    return -1;
-  } else {
-    return 0;
-  }
+function sortTodoByCreatedTimeAsc(objectA, objectB) {
+  return new Date(objectA.createdTime) - new Date(objectB.createdTime);
 }
 
-function sortTodoDescending(objectA, objectB) {
-  if (objectA < objectB) {
-    return -1;
-  } else if (objectA > objectB) {
-    return 1;
-  } else {
-    return 0;
-  }
+function sortTodoByCreatedTimeDesc(objectA, objectB) {
+  return new Date(objectB.createdTime) - new Date(objectA.createdTime);
+}
+
+function sortTodoByTitleAsc(objectA, objectB) {
+  if (objectA.title < objectB.title) return -1;
+  if (objectA.title > objectB.title) return 1;
+  return 0;
+}
+
+function sortTodoByTitleDesc(objectA, objectB) {
+  if (objectA.title < objectB.title) return 1;
+  if (objectA.title > objectB.title) return -1;
+  return 0;
 }
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  const [sortAsc, setSortAsc] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortAscByCreatedTime, setSortAscByCreatedTime] = useState(true);
+  const [sortAscByTitle, setSortAscByTitle] = useState(true);
 
   async function postData(newToDoTitle) {
     const options = {
@@ -63,6 +64,7 @@ function App() {
       const newTodo = {
         title: data.records[0].fields.title,
         id: data.records[0].id,
+        createdTime: data.records[0].createdTime,
       };
       setTodoList([newTodo, ...todoList]);
     } catch (error) {
@@ -81,9 +83,7 @@ function App() {
 
     const url = ` https://api.airtable.com/v0/${
       import.meta.env.VITE_AIRTABLE_BASE_ID
-    }/${
-      import.meta.env.VITE_TABLE_NAME
-    }?view=Grid%20view&sort[0][field]=title&sort[0][direction]=asc`;
+    }/${import.meta.env.VITE_TABLE_NAME}`;
 
     try {
       const response = await fetch(url, options);
@@ -94,13 +94,14 @@ function App() {
       const data = await response.json();
 
       const todos = data.records.map((record) => {
-        return { id: record.id, title: record.fields.title };
+        return {
+          id: record.id,
+          title: record.fields.title,
+          createdTime: record.createdTime,
+        };
       });
 
-      //Sorted
-      const sortedTodo = sortTodo(todos);
-
-      setTodoList(sortedTodo);
+      setTodoList(todos);
       setIsLoading(false);
     } catch (error) {
       console.log(error.message);
@@ -121,21 +122,36 @@ function App() {
     setTodoList(filterTodo);
   }
 
-  function sortTodo(todo) {
+  // My sort functions
+  function sortTodoByCreatedTime(todo) {
     return todo.sort((objectA, objectB) => {
-      if (sortAsc) {
-        return sortTodoAscending(objectA.title, objectB.title);
+      if (sortAscByCreatedTime) {
+        return sortTodoByCreatedTimeAsc(objectA, objectB);
       } else {
-        return sortTodoDescending(objectA.title, objectB.title);
+        return sortTodoByCreatedTimeDesc(objectA, objectB);
       }
     });
   }
 
-  function handleSortToggleClick() {
-    // const sortedTodo = sortTodo(todoList);
-    // setTodoList(sortedTodo)
-    setTodoList((prevTodo) => sortTodo(prevTodo));
-    setSortAsc(!sortAsc);
+  function sortTodoByTitle(todo) {
+    return todo.sort((objectA, objectB) => {
+      if (sortAscByTitle) {
+        return sortTodoByTitleAsc(objectA, objectB);
+      } else {
+        return sortTodoByTitleDesc(objectA, objectB);
+      }
+    });
+  }
+
+  // Button handlers
+  function handleSortByCreatedTimeClick() {
+    setTodoList((prevTodo) => sortTodoByCreatedTime(prevTodo));
+    setSortAscByCreatedTime(!sortAscByCreatedTime);
+  }
+
+  function handleSortByTitleClick() {
+    setTodoList((prevTodo) => sortTodoByTitle(prevTodo));
+    setSortAscByTitle(!sortAscByTitle);
   }
 
   return (
@@ -150,11 +166,16 @@ function App() {
 
                 <div className="sort-button-container">
                   <button
-                    onClick={handleSortToggleClick}
+                    onClick={handleSortByCreatedTimeClick}
                     className="sort-button"
                   >
-                    {" "}
-                    Change Sorting Order
+                    Toggle Sort by Created Time
+                  </button>
+                  <button
+                    onClick={handleSortByTitleClick}
+                    className="sort-button"
+                  >
+                    Toggle Sort by Title
                   </button>
                 </div>
 
